abd4d63b35a344f216cd39ad1b5c7c1cc832d708
CalvinL93 <calvinl93@yahoo.com>
2024-03-07 10:35:43 -0600

    Added background, changed text

--- Game Files/main.py
+++ Game Files/main.py
@@ -8,6 +8,8 @@ WIDTH, HEIGHT = 900, 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FONT = pygame.font.Font(None, 36)
+TEXT_COLOR = (255, 255, 255)
+OUTLINE_COLOR = (0, 0, 0)
 
 # we will run the questions from a seperate json file later when implementing the edit question screen
 questions = [
@@ -21,27 +23,43 @@ questions = [
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("ASTROQUIZ")
 
+background_image = pygame.image.load("Game Files/assets/images/menubackground.jpg")
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+
+button_rect = pygame.Rect(WIDTH // 2 - 50, HEIGHT // 2 - 24, 100, 50)
+
+def outline_text(text, height) :
+    text_outline = FONT.render(text, True, OUTLINE_COLOR)
+    rendered_text = FONT.render(text, True, WHITE)
+
+    screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 - 1, height))
+    screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 + 1, height))
+    screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2, height - 1))
+    screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2, height + 1))
+    screen.blit(rendered_text, (WIDTH // 2 - rendered_text.get_width() // 2, height))
+
+    button_rect = pygame.Rect(WIDTH // 2 - rendered_text.get_width() // 2 - 10, height - 7, rendered_text.get_width() + 20, 40)
+    pygame.draw.rect(screen, (255, 255, 255, 0), button_rect, 2)
+
+
 # We can make this clickable once we have the design of the menu page more complete
 def main_menu():
     # Display main menu options
     screen.fill(WHITE)
-    text = FONT.render("ASTROQUIZ", True, BLACK)
-    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, 100))
-    
-    play_text = FONT.render("Play Game (Press 'P')", True, BLACK)
-    screen.blit(play_text, (WIDTH // 2 - play_text.get_width() // 2, 200))
-    
-    high_scores_text = FONT.render("High Scores (Press 'H')", True, BLACK)
-    screen.blit(high_scores_text, (WIDTH // 2 - high_scores_text.get_width() // 2, 250))
-    
-    edit_questions_text = FONT.render("Edit Questions (Press 'E')", True, BLACK)
-    screen.blit(edit_questions_text, (WIDTH // 2 - edit_questions_text.get_width() // 2, 300))
+    screen.blit(background_image, (0, 0))
 
-    edit_questions_text = FONT.render("Edit Settings (Press 'S')", True, BLACK)
-    screen.blit(edit_questions_text, (WIDTH // 2 - edit_questions_text.get_width() // 2, 350))
+    # The game title needs to be drawn on the menu image so it will not look like this when done
+    outline_text("ASTROQUIZ", 100)
+    
+    outline_text("Play Game (Press 'P')", 200)
 
-    edit_questions_text = FONT.render("Quit (Press 'Q')", True, BLACK)
-    screen.blit(edit_questions_text, (WIDTH // 2 - edit_questions_text.get_width() // 2, 400))
+    outline_text("High Scores (Press 'H')", 250)
+
+    outline_text("Edit Questions (Press 'E')", 300)
+    
+    outline_text("Edit Settings (Press 'S')", 350)
+
+    outline_text("Quit (Press 'Q')", 400)
     
     pygame.display.flip()
 
