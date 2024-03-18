36d355fb95a36c5dd95ff949bfa85ff661be54d4
CalvinL93 <calvinl93@yahoo.com>
2024-03-18 14:58:01 -0500

    cleaned up settings.py

--- Game Files/settings.py
+++ Game Files/settings.py
@@ -1,43 +1,18 @@
 import pygame
 import utilities as u
 
+# needed for font setting
 pygame.init()
 
+# Game Settings
 WIDTH, HEIGHT = 900, 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FONT = pygame.font.Font(None, 36)
 TEXT_COLOR = (255, 255, 255)
-# OUTLINE_COLOR = (0, 0, 0)
 
 background_image = pygame.image.load("Game Files/assets/images/menubackground.jpg")
 background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
-
-# # Outlines text without box
-# def outline_text(screen, text, height) :
-#     text_outline = FONT.render(text, True, OUTLINE_COLOR)
-#     rendered_text = FONT.render(text, True, WHITE)
-
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 - 1, height))
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 + 1, height))
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2, height - 1))
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2, height + 1))
-#     screen.blit(rendered_text, (WIDTH // 2 - rendered_text.get_width() // 2, height))
-
-# def outline_text_w_box(screen, text, height, width=0) :
-#     text_outline = FONT.render(text, True, OUTLINE_COLOR)
-#     rendered_text = FONT.render(text, True, WHITE)
-
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 - 1 + width, height))
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 + 1 + width, height))
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 + width, height - 1))
-#     screen.blit(text_outline, (WIDTH // 2 - text_outline.get_width() // 2 + width, height + 1))
-#     screen.blit(rendered_text, (WIDTH // 2 - rendered_text.get_width() // 2 + width, height))
-
-#     button_rect = pygame.Rect(WIDTH // 2 - rendered_text.get_width() // 2 - 10 + width, height - 7, rendered_text.get_width() + 20, 40)
-#     pygame.draw.rect(screen, (255, 255, 255, 0), button_rect, 1)
-
-#     return button_rect
 
 def edit_settings(screen) :
     running = True
@@ -60,3 +35,4 @@ def edit_settings(screen) :
         #difficulty 
         #turn off questions 
         #resize the game page
+        #ship choice
